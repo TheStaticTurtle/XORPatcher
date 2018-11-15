6f6c5a997fb527314ccae3f4b223ab38a8d4643f
TurtleForGaming <tuglersamuel@gmail.com>
2018-11-15 14:07:58 +0100

    Fixed variable name

--- patcher.py
+++ patcher.py
@@ -4,11 +4,11 @@ def applyXOR(file):
 	try:
 		print("Applying XOR to  "+file)
 		reload(random)
-		random.seed(fname)
-		b = bytearray(open(fname, 'rb').read())
+		random.seed(file)
+		b = bytearray(open(file, 'rb').read())
 		for i in range(len(b)):
 			b[i] ^= random.randint(0,254)
-		open(fname, 'wb').write(b)
+		open(file, 'wb').write(b)
 	except IOError:
 		print("IOError while applying XOR to  "+file)
 if os.path.isfile(fname):
